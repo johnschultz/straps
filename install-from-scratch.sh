e913dfab5f89100e7ab70edce1b6806cee3eb454
Johnathon Schultz <johnathon.schultz@gmail.com>
2016-07-23 08:47:41 +0000

    Add cookbooks vendor and update install script

--- install-from-scratch.sh
+++ install-from-scratch.sh
@@ -2,5 +2,8 @@
 
 curl -LO https://omnitruck.chef.io/install.sh && sudo bash ./install.sh  && rm install.sh
 UNPRIV_USER=$(whoami)
+if [ ! -d "$DIRECTORY" ]; then
+    tar -xvzf cookbooks-vendor.tar.gz berks-cookbooks/
+fi
 sudo UNPRIV_USER=$UNPRIV_USER chef-client --local-mode -c config.rb -j solo.json
 
